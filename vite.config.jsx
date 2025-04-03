95938e8d229fccc8a7195c4e369e12fc00b5de40
jcva12345 <villaagustinjohncarlo@gmail.com>
2025-04-03 16:35:02 +0800

    initial commit

--- vite.config.jsx
+++ vite.config.jsx
@@ -1,37 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@components': path.resolve(__dirname, './src/components'),
-      '@styles': path.resolve(__dirname, './src/styles'),
-      '@assets': path.resolve(__dirname, './src/assets'),
-      '@context': path.resolve(__dirname, './src/context')
-    }
-  },
+  base: process.env.VITE_BASE_PATH || "/PORTV3",
   server: {
-    port: 3000,
-    open: true,
-    host: true
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: true,
-    minify: 'terser',
-    chunkSizeWarningLimit: 1500,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
-        }
-      }
-    }
+    host: '0.0.0.0',
+    port: process.env.PORT || 3000,
+    allowedHosts: ["https://my-portfolio-v38v.onrender.com"]
   }
-}) 
+})
